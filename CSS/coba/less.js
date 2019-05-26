43393aacdb4c4638e8f30ae3a8ea1f08a2a57414
amrrobb <amr.robb@gmail.com>
2019-05-26 10:41:41 +0700

    Learn Less

---
+++ CSS/coba/less.js
@@ -0,0 +1,13 @@
+<script>
+  less = {
+    env: "development",
+    async: false,
+    fileAsync: false,
+    poll: 1000,
+    functions: {},
+    dumpLineNumbers: "comments",
+    relativeUrls: false,
+    rootpath: ":/a.com/"
+  };
+</script>
+<script src="less.js"></script>
