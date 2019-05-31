67d95de4e44c94d86096e9d0f16ac882f6153ae6
amrrobb <amr.robb@gmail.com>
2019-06-01 00:19:55 +0700

    learn javascript

---
+++ JavaScript/Tutorial/datatype-if.js
@@ -0,0 +1,12 @@
+function datatype() {
+    var data = 3;
+    console.log(typeof(data));
+    
+    if (data == "3" ){
+        return 'success';
+    } else {
+        return "fail";
+    }
+}
+
+console.log(datatype());
---
+++ JavaScript/Tutorial/switch.js
@@ -0,0 +1,20 @@
+function test(x){
+    
+
+    switch(x){
+        case 1:
+            console.log("A");
+            break;
+        case 2:
+            console.log("B");
+             break;
+        case 3:
+            console.log("C");
+            break;
+        default: 
+            console.log("T");
+            
+    } 
+}
+
+test(4)
