bdce822e1b26313c675d1d4aee954884c33ba9d2
amrrobb <amr.robb@gmail.com>
2019-05-31 23:49:48 +0700

    learn javascript

---
+++ JavaScript/Tutorial/if-else-if.js
@@ -0,0 +1,11 @@
+var x;
+
+if(x<20)
+    console.log("Below 20")
+else if (x>20)
+    console.log("Above 20")
+else if (x===20)
+    console.log("Equals 20");
+else 
+    console.log("Number undefined");
+    
