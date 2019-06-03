18510c6fcd07e3cceea7849ca14e22bdc6c3d202
amrrobb <amr.robb@gmail.com>
2019-06-03 21:20:42 +0700

    learn javascript

---
+++ JavaScript/tutorial2/char-code.js
@@ -0,0 +1,15 @@
+var x = "Soto gulai enak rasanya";
+
+console.log(x.charCodeAt(5));
+
+y = String.fromCodePoint(65);
+console.log(y);
+
+y = String.fromCharCode(65);
+console.log(y);
+
+console.log(x.charAt(3));
+
+// y = x.charAt(3);
+// console.log(y);
+
---
+++ JavaScript/tutorial2/upper-lower.js
@@ -0,0 +1,10 @@
+var x = "Soto gulai enak rasanya";
+
+console.log(x.toLocaleLowerCase());
+console.log(x.toLocaleUpperCase());
+
+console.log(x.toLowerCase());
+console.log(x.toUpperCase());
+
+
+
